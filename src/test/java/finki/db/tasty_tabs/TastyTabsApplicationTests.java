700c4e06cc622248906302100c15abd8d3500209
Atanas Vitanov <atanas_vitanov@yahoo.com>
2025-07-11 15:03:19 +0200

    Initial

---
+++ src/test/java/finki/db/tasty_tabs/TastyTabsApplicationTests.java
@@ -0,0 +1,13 @@
+package finki.db.tasty_tabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyTabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
